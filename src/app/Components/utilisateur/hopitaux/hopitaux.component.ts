5218e867c779b4ba75b33c12f136307ec1df50c4
unknown <assanedip999@gmail.com>
2024-02-13 16:43:11 +0000

    affichage Image hopital ok

--- src/app/Components/utilisateur/hopitaux/hopitaux.component.ts
+++ src/app/Components/utilisateur/hopitaux/hopitaux.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HopitalService } from 'src/app/services/hopital.service';
 
 @Component({
   selector: 'app-hopitaux',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./hopitaux.component.scss']
 })
 export class HopitauxComponent {
+  constructor(private hopitalService: HopitalService) { }
+  ngOnInit(): void {
+    this.getAllHospitals();
+  }
+
+    Hopitaux: any[] = [];
+
+   getAllHospitals() {
+   this.hopitalService.getAllHospital().subscribe(
+      (Hopitaux) => {
+        // Afficher la liste des regions
+        console.log(Hopitaux);
+        this.Hopitaux = Hopitaux.hopitaux;
+
+        console.log(this.Hopitaux);
+     },
+     
+     
+
+      (error) => {
+        // Traiter l'erreur de liste
+
+      }
+    );
+  }
+
 
 }
